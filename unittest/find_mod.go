d62d3b248709d07d4c7c249d2f81e97622b4b351
benjawan.ru <benjawan.ru@tnis.com>
2020-09-14 17:30:08 +0700

    [refactor] code

--- unittest/find_mod.go
+++ unittest/find_mod.go
@@ -3,34 +3,17 @@ package unittest
 import "fmt"
 
 func findMod(input int) string {
-	// n := float64(input)
-	// if n == 0 {
-	// 	return fmt.Sprint(input)
-	// } else if math.Mod(n, 15) == 0 {
-	// 	return mod15(input)
-
-	// 	// return fmt.Sprint(input, " : FizzBuzz")
-	// } else if math.Mod(n, 5) == 0 {
-	// 	return fmt.Sprint(input, " : Buzz")
-	// } else if math.Mod(n, 3) == 0 {
-	// 	// return fmt.Sprint(input, " : Fizz")
-	// 	return "Fizz"
-	// } else {
-	// 	return fmt.Sprintf("%.0f", n)
-	// }
-	mod3, mod5 := mod15(input)
-	if mod3 && mod5 {
+	mod := [2]bool{input%5 == 0, input%3 == 0}
+	switch mod {
+	case [2]bool{true, true}:
 		return "FizzBuzz"
-	} else if mod5 {
+	case [2]bool{true, false}:
 		return "Fizz"
-	} else if mod3 {
+	case [2]bool{false, true}:
 		return "Buzz"
-	} else {
+	default:
 		return fmt.Sprintf("%d", input)
-	}
 
-}
-func mod15(input int) (bool, bool) {
-	return ((input % 3) == 0), ((input % 5) == 0)
+	}
 
 }
